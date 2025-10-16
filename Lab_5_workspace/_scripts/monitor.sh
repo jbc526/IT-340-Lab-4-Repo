7a6f27f4eb837bbae3fc0b18fce9e55d42df684e
jbc526 <jbc5@njit.edu>
2025-10-16 01:02:31 -0400

    Adding monitor.sh and timesheet.sh

---
+++ Lab_5_workspace/_scripts/monitor.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "System Monitor - $(date)" >> ..logs/system.log
+uptime >> ../logs/system.log
+free -h >> ../logs/system.log
+df -h >> ../logs/system.log
+echo "------------------------" >> ../logs/system.log
+
+
